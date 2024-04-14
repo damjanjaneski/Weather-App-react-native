93b296dc35b49eb26f83801fbfd659c795894ff2
damjanjaneski <djaneski10@gmail.com>
2024-04-14 22:03:37 +0200

    added missed logic for button and input disabling on search

--- src/app/index.js
+++ src/app/index.js
@@ -12,7 +12,7 @@ import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
 import { typing, reset } from "../features/weather";
 import { reset as resetForecast } from "../features/forecast";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { API_KEY } from "@env";
 import ResetReduxOnBackButton from "../features/ResetReduxOnBack";
 
@@ -20,8 +20,10 @@ export default function HomeScreen() {
   const navigation = useNavigation();
   const dispatch = useDispatch();
   const city = useSelector((store) => store.weather);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handlePress = () => {
+    setIsLoading(true);
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city.name}&appid=${API_KEY}`
@@ -29,6 +31,7 @@ export default function HomeScreen() {
       .then((response) => {
         navigation.navigate("city", { "": city.name });
         dispatch(enterCity(response.data));
+        setIsLoading(false);
       })
       .catch(() => {
         navigation.navigate("error", {});
@@ -52,9 +55,14 @@ export default function HomeScreen() {
         onChangeText={(value) => handleChange(value)}
         value={city.name}
         placeholder="Enter a city name"
+        editable={!isLoading}
       />
 
-      <TouchableOpacity onPress={handlePress} style={styles.btn}>
+      <TouchableOpacity
+        disabled={isLoading}
+        onPress={handlePress}
+        style={styles.btn}
+      >
         <Text style={{ color: "white" }}>Search</Text>
       </TouchableOpacity>
 
